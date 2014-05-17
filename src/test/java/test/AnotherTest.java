3c64321c983b8991d5ec5e81896397d537698525
miladhub <milad.bourhani@gmail.com>
2014-05-17 07:31:48 +0200

    refactored second test

--- src/test/java/test/AnotherTest.java
+++ src/test/java/test/AnotherTest.java
@@ -1,12 +1,9 @@
 package test;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class AnotherTest {
-@Test
-public void someTest() throws Exception {
-
-}
+	@Test
+	public void someTest() throws Exception {
+	}
 }
